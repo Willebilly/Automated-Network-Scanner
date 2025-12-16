82178530af4ce580fc4f5f629b358fc914f4227a
William <william.friden@gmail.com>
2025-12-16 16:07:32 +0100

    Programmet är nu körbart och uppfyller sin huvudfunktion.

--- Auto_Scan.sh
+++ Auto_Scan.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+# Diverse funktioner som var nödvändiga för koden.
+
+
 # Följande körs ifall man inte har en text fil som heter IP-list
 if [ ! -f IP-list.txt ]; then
 echo "ERROR: IP-list.txt är ej installerad."
 echo "Installerar: IP-list.txt"
-touch IP-list.txt #Creates the List in which our  neighbouring devices will be.
+touch IP-list.txt # Skapar listan som kommer att innehålla våra grann-enheters IP-adresser.
 echo "IP-list.txt installerad."
-echo ""
+else
+> IP-list.txt # Om listan redan finns töms den för användning
 
 fi
 
-echo "Peasant!"
+echo ""
+
+# Man måste veta vilka de första 3 oktetterna är i ens IP-adress range.
+my_ip=$(ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+') # Variabel som sparar IP-nätverket man är på som går ut mot internet.
+echo "Din IPv4 adress: $my_ip"
+oct="${my_ip%${my_ip##*.}}" # Tar bart den sista oktetten.
+for ip in $(seq 1 254); do  # Gör en Ping sweep från .1 - .254
+ping -c 1 $oct$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> IP-list.txt & # Själva sweepen, men också sparar i IP-list.txt
+done
+echo "IPv4 har sparats i IP-list.txt"
+
+while IFS= read -r line; do # While loop för att kunna skanna porten för varje IPv4 adress i listan.
+echo "Processing line: $line"
+nmap -T5 --min-rate 5000 "$line" # Använder nmap för att skanna IPv4 TCP port.
+
+wait
+echo ""
+done < IP-list.txt
--- README.md
+++ README.md
@@ -1,7 +1,20 @@
+2025-12-16
+
+Nu sparas användarens lokala IP adress i en variabel. Koden får fram IP genom den IP-route som går ut mot googles 8.8.8.8 adress, därefter tittar den på source IPv4-adressen som togs för att kunna nå dit. Jag planerar för att ta de första 3 oktetterna ur denna för att användas när ping sweep genomförs.
+
+Syftet med denna feature är att spara tid och göra så mycket som möjligt utan någon extra input från användaren.
+
+Därefter delade jag upp IPv4 adressen så att nätverksdelen sparas i variabeln my_ip.
+
+Använde mig av en for loop som både utför Ping sweep och sparar resultatet i IP-list.txt. Med hjälp av Ping sweep kan vi se vilka andra enheter som är med på nätverket användaren är uppkopplad till. 
+
+Senare i projektet kommer informationen i IP-list.txt att användas för en TCP-portscan.
+
+Programet använder sig nu av nmap för att skanna IPv4 adresserna efter öppna TCP-portar. 
+
 2025-12-15
 
 Jag har skrivit en bit med kod som körs ifall IP-list.txt inte redan finns
 på dator. Denna kommer användas senare i min kod som ska samla alla
 IP-adresser som används av enheter på det nätverk användaren är uppkopplad 
-på. 
-
+på.
