25928ee8e003466722252267f8c66344dc9ce519
William <william.friden@gmail.com>
2025-12-17 10:11:00 +0100

    Automated-Networkscanner 1.0

--- Auto_Scan.sh
+++ Auto_Scan.sh
@@ -11,10 +11,14 @@ touch IP-list.txt # Skapar listan som kommer att innehålla våra grann-enheters
 echo "IP-list.txt installerad."
 else
 > IP-list.txt # Om listan redan finns töms den för användning
-
 fi
 
-echo ""
+if [ ! -f /usr/bin/nmap ]; then # Om nmap är ej installerad kommer den att bli det här
+echo "ERROR: nmap är ej installerad."
+echo "Installerar: nmap"
+sudo apt update && sudo apt install nmap # Installerar nmap
+echo "nmap installerad."
+fi
 
 # Man måste veta vilka de första 3 oktetterna är i ens IP-adress range.
 my_ip=$(ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+') # Variabel som sparar IP-nätverket man är på som går ut mot internet.
@@ -24,6 +28,7 @@ for ip in $(seq 1 254); do  # Gör en Ping sweep från .1 - .254
 ping -c 1 $oct$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> IP-list.txt & # Själva sweepen, men också sparar i IP-list.txt
 done
 echo "IPv4 har sparats i IP-list.txt"
+echo ""
 
 while IFS= read -r line; do # While loop för att kunna skanna porten för varje IPv4 adress i listan.
 echo "Processing line: $line"
@@ -31,4 +36,4 @@ nmap -T5 --min-rate 5000 "$line" # Använder nmap för att skanna IPv4 TCP port.
 
 wait
 echo ""
-done < IP-list.txt
+done < IP-list.txt # Visar att den ska hämta från IP-list.txt
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-2025-12-16
+# 2025-12-17
+Idag är Automated-Networkscanner 1.0 färdig!
+
+Jag har lagt till en if sats som ska leta efter filen nmap. Om den inte finns kommer programmet att ladda ner det. 
+
+# 2025-12-16
 
 Nu sparas användarens lokala IP adress i en variabel. Koden får fram IP genom den IP-route som går ut mot googles 8.8.8.8 adress, därefter tittar den på source IPv4-adressen som togs för att kunna nå dit. Jag planerar för att ta de första 3 oktetterna ur denna för att användas när ping sweep genomförs.
 
@@ -12,7 +17,7 @@ Senare i projektet kommer informationen i IP-list.txt att användas för en TCP-
 
 Programet använder sig nu av nmap för att skanna IPv4 adresserna efter öppna TCP-portar. 
 
-2025-12-15
+# 2025-12-15
 
 Jag har skrivit en bit med kod som körs ifall IP-list.txt inte redan finns
 på dator. Denna kommer användas senare i min kod som ska samla alla
