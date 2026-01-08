e74ddc44ccecfc6aa19387a29dbcd98481a0b68b
ZanePersona <48486382+Willebilly@users.noreply.github.com>
2026-01-08 14:59:17 +0100

    Automated-Network-Scanner v1.5
    Enhance Auto_Scan.sh with help and version options

--- Auto_Scan.sh
+++ Auto_Scan.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 set -euo pipefail
-if [ "$EUID" -ne 0 ]; then
+
+show_help() { # "Help" flaggans funktion
+    cat << EOF
+Usage: sudo bash Auto_Scan.sh [OPTIONS]
+
+Options:
+  -h, --help        Visar detta hjälp meddelande
+  -v, --version     Visar vilken version av programmet du använder + lite extra info :)
+EOF
+}
+
+for arg in "$@"; do # lägger till några argument här för att hjälpa användaren!!!
+    case "$arg" in
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        -v|--version)
+            cat README.md
+            exit 0
+            ;;
+    esac
+done
+
+
+if [ "$EUID" -ne 0 ]; then # Vad har programmet för behörighet?
   echo "Kör scriptet med sudo: sudo bash Auto_Scan.sh"
   echo "Program avslutas."
   exit 1
 fi
 
-if ! ping -c1 www.google.com >/dev/null 2>&1 ; then
+if ! ping -c1 www.google.com >/dev/null 2>&1 ; then # Kollar på användares nätverks status, har de någon till att börja med?
   echo "Ingen internet uppkoppling"
+  echo "Se över din internet uppkoppling. Har du fått en ip adress?"
   echo "Program avslutas."
   exit 1
 fi
@@ -23,14 +49,15 @@ fi
 
 # Funktionen tar bort IP-list.txt när scriptet avslutas eller om något gick snett.
 end_func() {
-if [ $? -eq 0 ]; then 																# Script fungerar och avslutades normalt!
+if [ $? -eq 0 ]; then # Script fungerar och avslutades normalt!
   rm IP-list.txt
-else 																				# Script kraschar
+else # Script kraschar
   echo "Något gick snett!"
   rm IP-list.txt
 fi
 }
-trap end_func EXIT						 											# När koden avslutas eller om något går fel körs funktionen
+
+trap end_func EXIT # När koden avslutas eller om något går fel körs funktionen
 
 
 # Följande körs ifall man inte har en text fil som heter IP-list
@@ -52,7 +79,7 @@ oct="${my_ip%${my_ip##*.}}" 														# Tar bart den sista oktetten.
 for ip in $(seq 1 254); do  														# Gör en Ping sweep från .1 - .254
 ping -c 1 $oct$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> IP-list.txt & 	# Själva sweepen, men också sparar i IP-list.txt
 done
-wait # Väntar tills alla ping-kommandon är klara innan den går vidare.
+wait 																				# Väntar tills alla ping-kommandon är klara innan den går vidare.
 echo ""
 echo "=================================================="
 echo ""
